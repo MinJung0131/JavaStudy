85f8ac6a8d9218d2eaf1f437a2fba6d1621fc5b8
MinJung0131 <77926129+MinJung0131@users.noreply.github.com>
2024-03-21 18:15:52 +0900

    Create Hello.java

---
+++ chap01/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+/*
+ 작성자 : 박민정
+ 작성일 2024.03.21
+ */
+
+//프로그램 실행 진입점
+public class Hello {
+	//콘솔에 출력하는 실행문
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+
+	}//end of main
+
+}//end of class
