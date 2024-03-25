7f1dd18fc12b89ad31996b7184254db06d5878aa
MinJung0131 <77926129+MinJung0131@users.noreply.github.com>
2024-03-25 22:27:28 +0900

    Create EscapeExample.java

---
+++ chap02/sec02/exam06/EscapeExample.java
@@ -0,0 +1,15 @@
+package sec02.exam06;
+
+public class EscapeExample {
+
+	public static void main(String[] args) {
+		System.out.println("번호\t이름\t직업");
+		System.out.println("행 단위 출력\n");
+		System.out.println("행 단위 출력\n");
+		System.out.println("우리는 \"개발자\" 입니다.");
+		System.out.println("봄\\여름\\가을\\겨울");
+		System.out.println("캐리지 리턴 \r 캐리지 리턴");
+		System.out.println("유니 코드 출력 \uac00");
+	}
+
+}
